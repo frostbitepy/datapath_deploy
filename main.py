b9084600c1aa349c718e64f0ad8c34639d475592
frostbitepy <fran94rv@gmail.com>
2023-11-23 19:14:10 -0300

    Updated main file

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+import argparse
+import pandas as pd
+import pickle
 from fastapi import FastAPI
 from routes import router
 
-app = FastAPI()
+# Load the trained model using pickle
+with open('modelo_entrenado.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 
-app.include_router(router)
+def make_predictions(model, input_data):
+    # Make predictions using the model
+    predictions = model.predict(input_data)
+    return predictions
+
+if __name__ == "__main__":
+    
+    app = FastAPI()
+
+    app.include_router(router)
+    
+    parser = argparse.ArgumentParser(description="Predict Pulsars from a CSV file")
+    parser.add_argument("input_data", help="Path to the input CSV file containing data for prediction")
+
+    args = parser.parse_args()
+
+    # Load the input data
+    input_data = pd.read_csv(args.input_data)
+
+    # Make predictions
+    predictions = make_predictions(loaded_model, input_data)
+
+    # Print the predictions
+    print("Predictions:")
+    print(predictions)
